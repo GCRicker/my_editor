5b2927b561e9782bc92fb8702e9d168cf04bf57e
Gregs Laptop <GCRicker@comcast.net>
2021-02-09 23:49:58 -0500

    quick edit

--- Greg_Editor.py
+++ Greg_Editor.py
@@ -46,7 +46,7 @@ window_height = 500
 screen_width = window.winfo_screenwidth()       # The total screen width in pixels
 screen_height = window.winfo_screenheight()     # The total screen height in pixels
 x = int((screen_width/2) - (window_width/2))    # Have the windows center in the screen's center
-y = int((screen_height/2) - (window_height/2))    # Have the windows center in the screen's center
+y = int((screen_height/2) - (window_height/2))  # Have the windows center in the screen's center
 window.geometry("{}x{}+{}+{}".format(window_width, window_height, x, y))
 
 
